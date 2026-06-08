c3f664f01b9ae948c0103033982eff928ee282fa
Hackiton <ghost.raftar@gmail.com>
2026-06-08 20:50:23 +0530

    Implement reveal on scroll functionality
    Add scroll event listener to reveal elements

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ behavior:"smooth"
 })
 
 }
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll",()=>{
+
+reveals.forEach(item=>{
+
+let top = item.getBoundingClientRect().top;
+
+let windowHeight = window.innerHeight;
+
+if(top < windowHeight-100){
+
+item.classList.add("active");
+
+}
+
+})
+
+})
